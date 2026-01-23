144883b0928f48605a54e4b817214176d123edae
TSheyd <sheydaevtimur@yandex.ru>
2026-01-23 22:18:04 +0700

    *

--- packages/config-plane/config_plane/__init__.py
+++ packages/config-plane/config_plane/__init__.py
@@ -3,11 +3,6 @@ from .impl.memory import create_memory_config_repo
 from .impl.git import create_git_config_repo
 from .impl.sql import (
     create_sql_config_repo,
-    get_branch_head,
-    set_branch_head,
-    get_snapshot_blob,
-    commit_if_changed,
-    snapshot_exists,
 )
 
 __all__ = [
@@ -18,9 +13,4 @@ __all__ = [
     "create_memory_config_repo",
     "create_git_config_repo",
     "create_sql_config_repo",
-    "get_branch_head",
-    "set_branch_head",
-    "get_snapshot_blob",
-    "commit_if_changed",
-    "snapshot_exists",
 ]
