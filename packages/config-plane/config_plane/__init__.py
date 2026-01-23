1096bcecfb7609cc15dce972b9d667e223938851
TSheyd <sheydaevtimur@yandex.ru>
2026-01-23 22:20:59 +0700

    *

--- packages/config-plane/config_plane/__init__.py
+++ packages/config-plane/config_plane/__init__.py
@@ -1,9 +1,7 @@
 from .base import ConfigSnapshot, ConfigStage, Blob, ConfigRepo
 from .impl.memory import create_memory_config_repo
 from .impl.git import create_git_config_repo
-from .impl.sql import (
-    create_sql_config_repo,
-)
+from .impl.sql import create_sql_config_repo
 
 __all__ = [
     "ConfigSnapshot",
