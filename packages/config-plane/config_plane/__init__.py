7d47e90af0ce5fd9f7263c886d14bdc47f0ae3c5
TSheyd <sheydaevtimur@yandex.ru>
2026-01-23 18:47:06 +0700

    add sql store helpers

--- packages/config-plane/config_plane/__init__.py
+++ packages/config-plane/config_plane/__init__.py
@@ -1,6 +1,14 @@
 from .base import ConfigSnapshot, ConfigStage, Blob, ConfigRepo
 from .impl.memory import create_memory_config_repo
 from .impl.git import create_git_config_repo
+from .impl.sql import (
+    create_sql_config_repo,
+    get_branch_head,
+    set_branch_head,
+    get_snapshot_blob,
+    commit_if_changed,
+    snapshot_exists,
+)
 
 __all__ = [
     "ConfigSnapshot",
@@ -9,4 +17,10 @@ __all__ = [
     "ConfigRepo",
     "create_memory_config_repo",
     "create_git_config_repo",
+    "create_sql_config_repo",
+    "get_branch_head",
+    "set_branch_head",
+    "get_snapshot_blob",
+    "commit_if_changed",
+    "snapshot_exists",
 ]
